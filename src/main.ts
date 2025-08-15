c89aa9e9863a8593d6c2f426302334cd828acf4a
wcuadrosuniandes <w.cuadrosr@uniandes.edu.co>
2025-08-15 17:24:54 -0500

    fix: Se agrega permisos Cors

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
@@ -5,6 +6,14 @@ import { VersioningType } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Habilitar CORS para todos los dominios
+  app.enableCors({
+    origin: '*', // Permite cualquier origen
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
